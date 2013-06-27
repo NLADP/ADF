e18d7425304a06c8e9ce9701fd70dfcc35a3ca2c
rboeije <rboeij@yahoo.com>
2013-06-27 13:54:33 +0200

    ci test

--- Adf.Core.Tests/StringExtensionsTest.cs
+++ Adf.Core.Tests/StringExtensionsTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Adf.Core.Tests
 {
-    [TestClass]
+    [TestClass] 
     public class StringExtensionsTest
     {
         [TestMethod]
