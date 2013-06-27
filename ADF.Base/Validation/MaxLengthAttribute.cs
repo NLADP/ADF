cfb829ffa1a4817a806eeec287266bb24224b423
rboeije <rboeij@yahoo.com>
2013-06-27 11:02:03 +0000

    added solution

--- ADF.Base/Validation/MaxLengthAttribute.cs
+++ ADF.Base/Validation/MaxLengthAttribute.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Reflection;
-using Adf.Base.Domain;
 using Adf.Core.Extensions;
 using Adf.Core.Validation;
 
@@ -13,7 +12,6 @@ namespace Adf.Base.Validation
     public sealed class MaxLengthAttribute : Attribute, IPropertyValidator
     {
         private readonly int length;
-        private readonly int decimalLength;
 
         /// <summary>
         /// Creates a new <see cref="MaxLengthAttribute"/> instance with the supplied length.
@@ -22,18 +20,6 @@ namespace Adf.Base.Validation
         public MaxLengthAttribute(int length)
         {
             this.length = length;
-        }
-
-        /// <summary>
-        /// Creates a new <see cref="MaxLengthAttribute"/> instance with the supplied length.
-        /// Implemented only for the value object; Money.
-        /// </summary>
-        /// <param name="length">The supplied length.</param>
-        /// <param name="decimallength">The supplied length for value after the decimal delimiter.</param>
-        public MaxLengthAttribute(int length, int decimallength)
-        {
-            this.length = length;
-            decimalLength = decimallength;
         }
 
         /// <summary>
@@ -46,29 +32,9 @@ namespace Adf.Base.Validation
         /// </returns>
         public ValidationResult IsValid(PropertyInfo propertyToValidate, object value)
         {
-            if (value == null || value.ToString() == string.Empty) return ValidationResult.Success;
+            if (value == null) return ValidationResult.Success;
 
-            if (decimalLength != 0)
-                return !HasMaxLength(value, length)
-                     ? ValidationResult.CreateError(propertyToValidate, "Adf.Business.AttributeMaxLengthMoneyInvalid", propertyToValidate.Name, length, decimalLength)
-                     : ValidationResult.Success;
-
-            return !value.ToString().HasMaxLength(length)
-                ? ValidationResult.CreateError(propertyToValidate, "Adf.Business.AttributeMaxLengthInvalid", propertyToValidate.Name, length)
-                : ValidationResult.Success;
-        }
-
-        private static bool HasMaxLength(object value, int length)
-        {
-            Money money;
-            Money.TryParse(value.ToString(), out money);
-
-            if (money != null && money.Amount.HasValue)
-            {
-                if (money.Amount.Value < (decimal)Math.Pow(10, length))
-                    return true;
-            }
-            return false;
+            return !value.ToString().HasMaxLength(length) ? ValidationResult.CreateError(propertyToValidate, "Adf.Business.AttributeMaxLengthInvalid", propertyToValidate.Name, length) : ValidationResult.Success;
         }
 
         /// <summary>
@@ -76,15 +42,7 @@ namespace Adf.Base.Validation
         /// </summary>
         public int Length
         {
-            get { return length; }
-        }
-
-        /// <summary>
-        /// Returns the maximum length for value after the decimal delimiter.
-        /// </summary>
-        public int DecimalLength
-        {
-            get { return decimalLength; }
+            get { return (int)length; }
         }
     }
 }
